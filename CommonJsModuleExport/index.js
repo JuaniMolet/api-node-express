2f651b2b96ba95cb8d326d04434572a489836e5f
Juan Ignacio Molet <juanignacio.molet@4rsoluciones.com>
2024-05-06 13:00:16 -0300

    Información agregada sobre las extensiones de los archivos

--- CommonJsModuleExport/index.js
+++ CommonJsModuleExport/index.js
@@ -1,3 +1,4 @@
-//CommonJS require module
+//CommonJS require module.
+//No es necesario poner la extension. Porque Node.js busca por defecto un archivo con la extensión .js, .json o .node.
 const { sum } = require('./sum');
 console.log(sum(1, 2)); // 3
--- ESModuleExport/index.mjs
+++ ESModuleExport/index.mjs
@@ -1,5 +1,6 @@
 // .js -> por defecto utiliza CommonJS
 // .mjs -> para utilizar ES Modules
+//Es necesario poner la extension del archivo. Porque Node.js busca por defecto un archivo con la extensión .js, .json o .node.
 import { sum } from './sum.mjs';
 
 console.log(sum(1, 2)); // 3
