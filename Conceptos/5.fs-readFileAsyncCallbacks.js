bc6b0dbaa427d1cdbfbaf8b2184a54a873e43fd7
Juan Ignacio Molet <consultant-juan-ignacio@nftydoor.com>
2024-05-25 12:02:17 -0300

    new dev dependency added

--- Conceptos/5.fs-readFileAsyncCallbacks.js
+++ Conceptos/5.fs-readFileAsyncCallbacks.js
@@ -1,4 +1,4 @@
-//Script que lee dos archivos de texto y muestra su contenido en consola de manera asincrona.
+// Script que lee dos archivos de texto y muestra su contenido en consola de manera asincrona.
 // Ser asincrono significa que el programa no se detiene y sigue ejecutando otras operaciones mientras se lee el archivo.
 // Se llama Callback a la operación que se ejecuta cuando se completa una función asincrona.
 const fs = require("node:fs");
