924f8cb5a596b6c3c030476136b8a657fa58652e
Juan Ignacio Molet <consultant-juan-ignacio@nftydoor.com>
2024-06-03 13:30:29 -0300

    Add new image in http

--- 1.http.js
+++ 1.http.js
@@ -1,4 +1,5 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const desiredPort = process.env.PORT ?? 1234;
 
@@ -10,6 +11,17 @@ const server = http.createServer((req, res) => {
   } else if (req.url === "/about") {
     res.statusCode = 200;
     res.end("Acerca de nosotros");
+  } else if (req.url === "/imagen.png") {
+    fs.readFile("./imagen.png", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("No se encontró la imagen");
+      } else {
+        res.setHeader("Content-Type", "image/png");
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
   } else {
     res.statusCode = 404;
     res.end("Página no encontrada");
