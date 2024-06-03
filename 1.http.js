d6b684802b773bd2106b6ecf1d886f115092afd0
Juan Ignacio Molet <consultant-juan-ignacio@nftydoor.com>
2024-06-03 13:06:07 -0300

    Manejor de rutas

--- 1.http.js
+++ 1.http.js
@@ -3,8 +3,17 @@ const http = require("node:http");
 const desiredPort = process.env.PORT ?? 1234;
 
 const server = http.createServer((req, res) => {
-  console.log("Req:   ", req.url);
-  res.end("Hello World");
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Bienvenido a la página de inicio");
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.end("Acerca de nosotros");
+  } else {
+    res.statusCode = 404;
+    res.end("Página no encontrada");
+  }
 });
 
 server.listen(desiredPort, () => {
